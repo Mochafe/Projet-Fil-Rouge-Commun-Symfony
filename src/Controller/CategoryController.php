dfe471e8a85ab14b450fda75d1a28e9b7ba0fb4b
Florian Pasquet <florianpasquett@gmail.com>
2022-11-23 09:30:42 +0100

    correction

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,17 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+
 
 #[Route('/category', name: 'category')]
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index( CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
 
         $categories = $categoryRepository->findAll();
@@ -25,7 +24,7 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/{category}', name: 'subcategory')]
-    public function sub( Category $category): Response
+    public function sub(Category $category): Response
     {
      
       
